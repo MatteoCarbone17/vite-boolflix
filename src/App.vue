d4c9e110ac7348e61f671f19c38820f208e4d14e
MatteoCarbone17 <matteocarbone17@live.com>
2023-01-14 17:14:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import { store } from '../src/store.js'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      return:
+      store,
+      ApiUrl: 'https://api.themoviedb.org/3/movie/550?api_key=d375deb50bb5135ee140c55f9476e44c'
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get(this.ApiUrl, {
+
+      })
+        .then((response) => {
+
+        console.log(response.data)
+          // console.log(this.store.data)
+
+        });
+    },
+
+  },
+
+created(){
+     this.getApi()}
+
+}
+
+</script>
+
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './components/style/general.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
+</style>
